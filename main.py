159074feb1409a82cb1437612c803c1654cb71e3
DLOzTOR <zafabar1@gmail.com>
2022-12-08 09:03:10 +0200

    monor update

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from telethon.sync import TelegramClient,events
 api_id = #тут число
 api_hash = ""
 client = TelegramClient('AndrewHelicopterParcer', api_id, api_hash).start()
-channel = ["@writeBotMassage"]#можно установить несколько
+channel = [""]#можно установить несколько
 sendTo = ""
 
 
